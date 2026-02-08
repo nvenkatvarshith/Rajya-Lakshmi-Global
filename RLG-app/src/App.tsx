a82f6938ddc0ac27ad9df92e655e15af1e7c36c5
nvenkatvarshith <venkatvarshith2@gmail.com>
2026-02-08 16:34:34 +0530

    basic setup

---
+++ RLG-app/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navbar  from './components/Navbar.tsx'
+import Footer from './components/Footer.tsx'
+
+function App() {
+
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
